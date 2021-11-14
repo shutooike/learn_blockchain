21631c81bc412e7705149dd8996e34c83cbc99c7
shutooike <shutofootball@icloud.com>
2021-11-14 21:42:14 +0900

    データベースを実装。ウォレットを保存できるように

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem 'ecdsa'
 gem 'base58'
+gem 'redis'
--- wallet.rb
+++ wallet.rb
@@ -2,31 +2,41 @@ require 'ecdsa'
 require 'securerandom'
 require 'digest'
 require 'base58'
+require './database.rb'
 
 class Wallet
-  attr_reader :public_key
+  DB_KEY_PREFIX = 'wallet::'.freeze
+
+  attr_reader :public_key, :address
+
+  def self.init_from_db(address)
+    Database.new.restore(DB_KEY_PREFIX + address)
+  end
 
   def initialize
     group = ECDSA::Group::Secp256k1
 
     @private_key = 1 + SecureRandom.random_number(group.order - 1)
     @public_key = group.generator.multiply_by_scalar(private_key)
+    @address = _address
   end
 
-  def address
-    @address if @address
+  def save
+    key = DB_KEY_PREFIX + address
+    Database.new.save(key, self)
+  end
 
+  private
+
+  attr_reader :private_key
+
+  def _address
     compressed_public_key = prefix + public_key.x.to_s(16)
     hashed_public_key = double_hash(compressed_public_key)
     hashed_public_key_with_network_byte = '00' + hashed_public_key
     row_address = hashed_public_key_with_network_byte + checksum(hashed_public_key_with_network_byte)
-    @address = Base58.binary_to_base58([row_address].pack('H*'), :bitcoin)
-
-    @address
+    Base58.binary_to_base58([row_address].pack('H*'), :bitcoin)
   end
-
-  private
-  attr_reader :private_key
 
   def prefix
     public_key.y.even? ? '02' : '03'
